8adbb3faa78dfd9229b63ad20249ca5d729f2d63
Camcam <160264220+Kkammb3rry@users.noreply.github.com>
2024-03-08 11:23:22 -0600

    Function

---
+++ funcion.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int numero = 10;
+main(){
+ printf("\t\tCamila Herrera Martinez\n");
+ saludo();
+ printf("El valor de numero de FUNCION MAIN es de: %d", numero);
+}
+
+void saludo(){
+ printf("Amiga te cuerniaron\n");
+ printf("El valor de numero de FUNCION SALUDO es de: %d\n", numero);
+}
